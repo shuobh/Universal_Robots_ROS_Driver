2af92a8f35856aad5ed1503d8cfa7206bd9f63f6
Shuo <shuo@bluehill.coffee>
2025-01-20 16:21:46 -0800

    Integrated Inspire V2 driver

---
+++ ur_robot_driver/include/inspire_hand/hand_control_lib_v2.h
@@ -0,0 +1,119 @@
+#ifndef HAND_CONTROL_LIB_V2_H
+#define HAND_CONTROL_LIB_V2_H
+
+#include <ros/ros.h>
+#include <modbus/modbus.h>
+#undef TRUE
+#undef FALSE
+#include <sensor_msgs/JointState.h>
+
+// ...其他服务头文件...
+#include <mutex>
+
+namespace inspire_hand {
+// Define the maximum and minimum angle limits for the hand
+const double angle_upper_limit[] = {1.47, 1.47, 1.47, 1.47, 0.6, 1.308};
+const double angle_lower_limit[] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+
+class hand_serial {
+public:
+    hand_serial() {};
+
+    ~hand_serial();
+
+    void initialize(int hand_id, std::string ip_address, int port);
+
+    bool get_error();
+    
+    bool get_actual_force();
+    
+    bool get_actual_current();
+    
+    bool get_set_angle();
+                              
+    bool get_actual_angle();
+                                 
+    bool get_set_force();
+    
+    bool get_temp();
+    
+    bool get_set_position();
+                            
+    bool get_actual_position();
+                            
+    bool get_status();
+                                                                                                                    
+    bool set_clear_error();         
+                              
+    bool set_id(int id);
+
+    bool set_redu_ratio(int redu_ratio);
+    
+    bool set_position(const double pos[6]);
+    
+    bool set_gesture_number(int gesture_no);
+    
+    bool set_speed(const double speed[6]);
+                          
+    bool set_default_speed(const double speed[6]);
+    
+    bool set_angle(const double angle[6]);
+                      
+    bool set_force_calibration();
+                              
+    bool set_force(const double force[6]); 
+                          
+    bool set_default_force(const double force[6]);
+                                  
+    bool set_current_limit(const double current_limit[6]);
+    
+    bool set_reset_parameters();
+                     
+    int writeMultipleRegisters(int start_addr, const uint16_t* values, int num_values);
+    int getRegisterValue(int reg_addr) {
+        return readRegister(reg_addr); // 通过公有方法调用私有方法
+    }
+    // ...其他回调函数...
+
+    //hand state variables              
+    int hand_id_;
+    double curpos_[6];
+    double curangle_[6];
+    double curspeed_[6] = {0.0};
+    double curforce_[6];
+    double current_[6];
+    double errorvalue_[6];
+    int statusvalue_[6];
+    int tempvalue_[6];
+    double setpos_[6];
+    double setangle_[6];
+    double setangle_cmd_[6];
+    double setforce_[6];
+
+private:
+    // Modbus TCP 上下文
+    modbus_t *ctx_;
+    std::mutex cmd_mutex_;
+
+    // 设备参数
+    std::string ip_address_;
+    int port_;
+
+    // ...其他设置函数...
+    // 读取和写入 Modbus 数据的通用方法
+    int readRegister(int reg_addr);
+    int readRegisters(int reg_addr, int num_registers, uint16_t *tab_reg);
+    int writeRegister(int reg_addr, int value);
+
+
+    bool validate_values(const double values[6], double lower_limit, double upper_limit);
+    bool set_reg(const double values[6], int pin);
+    void get_reg(double (&values)[6], int pin);
+    bool save_setting();
+
+};
+
+} // namespace inspire_hand
+
+#endif
+
---
+++ ur_robot_driver/src/hand_control_lib_v2.cpp
@@ -0,0 +1,447 @@
+#ifndef HAND_CONTROL_LIB_V2_CPP
+#define HAND_CONTROL_LIB_V2_CPP
+
+#include <inspire_hand/hand_control_lib_v2.h>
+#include <ros/ros.h>
+
+namespace inspire_hand {
+// Destructor
+hand_serial::~hand_serial()
+{
+    if (ctx_) {
+        modbus_close(ctx_);
+        modbus_free(ctx_);
+    }
+}
+
+void hand_serial::initialize(int hand_id, std::string ip_address, int port) {
+    // Initialize Modbus TCP context
+    hand_id_ = hand_id;
+    ip_address_ = ip_address; // Replace with your device's IP
+    port_ = port; // Default Modbus TCP port
+    ctx_ = modbus_new_tcp(ip_address_.c_str(), port_);
+    
+    if (modbus_connect(ctx_) == -1) {
+        ROS_ERROR("Unable to connect to Modbus server: %s", modbus_strerror(errno));
+        modbus_free(ctx_);
+        ctx_ = nullptr;
+    }
+
+    ROS_INFO("Connected to Modbus server at %s:%d", ip_address_.c_str(), port_);
+}
+
+void hand_serial::get_reg(double (&values)[6], int pin) {
+    for(int i = 0; i < 6; i++) {
+        values[i] = readRegister(pin + i * 2);
+    }
+}
+
+// Callback to get error information
+bool hand_serial::get_error() {
+    ROS_INFO("Hand: Get error request received");
+
+    uint16_t tab_reg[6]; // 用于存储读取的寄存器值
+
+    // 读取寄存器 (地址从 1606 开始)
+    int rc = readRegisters(1606, 6, tab_reg);
+    if (rc == -1) {
+        ROS_ERROR("Failed to read error registers: %s", modbus_strerror(errno));
+        return true; // 返回成功
+    }
+
+    // 解析故障信息并存储到响应中
+    for (int i = 0; i < 3; i++) {
+        // 每个寄存器包含两个字节
+        errorvalue_[i * 2] = static_cast<float>(tab_reg[i] & 0xFF);    // 低字节在前
+        errorvalue_[i * 2 + 1] = static_cast<float>((tab_reg[i] >> 8) & 0xFF); // 高字节在后
+        
+        // 输出故障信息
+        ROS_INFO("ERROR(%d): %f", i * 2, errorvalue_[i * 2]);         // 输出低字节
+        ROS_INFO("ERROR(%d): %f", i * 2 + 1, errorvalue_[i * 2 + 1]); // 输出高字节
+    }
+
+    return true; // 返回成功
+}
+
+bool hand_serial::get_status() {
+    ROS_INFO("Hand: Get status request received");
+
+    uint16_t tab_reg[6]; // 用于存储读取的寄存器值
+
+    // 读取寄存器 (地址从 1612 开始)
+    int rc = modbus_read_registers(ctx_, 1612, 6, tab_reg);
+    if (rc == -1) {
+        ROS_ERROR("Failed to read status registers: %s", modbus_strerror(errno));
+        return true; // 返回成功
+    }
+
+    // 解析状态信息并存储到响应中
+    for (int i = 0; i < 3; i++) {
+        // 每个寄存器包含两个字节
+        statusvalue_[i * 2] = static_cast<int16_t>(tab_reg[i] & 0xFF);    // 低字节在前
+        statusvalue_[i * 2 + 1] = static_cast<int16_t>((tab_reg[i] >> 8) & 0xFF); // 高字节在后
+        
+        // 输出状态信息
+        ROS_INFO("STATUS(%d): %d", i * 2, statusvalue_[i * 2]);         // 输出低字节
+        ROS_INFO("STATUS(%d): %d", i * 2 + 1, statusvalue_[i * 2 + 1]); // 输出高字节
+    }
+
+    return true; // 返回成功
+}
+
+bool hand_serial::get_actual_force() {
+    ROS_INFO("Hand: Get Force Actual values request received");
+
+    // 直接读取各个手指的实际受力值
+    get_reg(curforce_, 1582); // 从寄存器 1582 开始读取
+
+    return validate_values(curforce_, 0, 3000);
+}
+
+bool hand_serial::get_actual_current() {
+    ROS_INFO("Hand: Get Current values request received");
+
+    // 直接读取各个电缸的电流值
+    get_reg(current_, 1594); // 从寄存器 1594 开始读取
+
+    return validate_values(current_, 0, 1000);
+}
+
+bool hand_serial::get_set_angle() {
+    ROS_INFO("Hand: Get Angle Set values request received");
+
+    // 直接读取各个手指的上电初始角度
+    get_reg(setangle_, 1486); // 从寄存器 1486 开始读取
+
+    return validate_values(setangle_, 0, 2000); // 检查读取的值是否有效
+}
+
+bool hand_serial::get_actual_angle() {
+    ROS_INFO("Hand: Get Angle Actual values request received");
+
+    // 直接读取各个手指的角度实际值
+    get_reg(curangle_, 1546); // 从寄存器 1546 开始读取
+
+    return validate_values(curangle_, 0, 2000); // 检查读取的值是否有效
+}
+
+bool hand_serial::get_set_force() {
+    ROS_INFO("Hand: Get Force Set values request received");
+
+    // 直接读取各个手指的力控设置值
+    get_reg(setforce_, 1498); // 从寄存器 1498 开始读取
+
+    return validate_values(setforce_, 0, 3000); // 检查读取的值是否有效
+}
+
+bool hand_serial::get_temp()
+{
+    ROS_INFO("Hand: Get temperature request received");
+
+    uint16_t tab_reg[6]; // 用于存储读取的寄存器值
+
+    // 读取寄存器 (地址从 1618 开始)
+    int rc = readRegisters(1618, 6, tab_reg);
+    if (rc == -1) {
+        ROS_ERROR("Failed to read temperature registers: %s", modbus_strerror(errno));
+        return true; // 返回成功
+    }
+
+    // 解析温度值并存储到响应中
+    for (int i = 0; i < 3; i++) {
+        // 每个寄存器包含两个字节
+        tempvalue_[i * 2] = static_cast<int16_t>(tab_reg[i] & 0xFF);       // 低字节;
+        tempvalue_[i * 2 + 1] = static_cast<int16_t>((tab_reg[i] >> 8) & 0xFF); // 高字节
+        
+        // 输出温度信息
+        ROS_INFO("TEMP(%d): %d", i * 2, tempvalue_[i * 2]);      // 输出低字节
+        ROS_INFO("TEMP(%d): %d", i * 2 + 1, tempvalue_[i * 2 + 1]); // 输出高字节
+    }
+
+    return true; // 返回成功
+}
+
+bool hand_serial::get_set_position() {
+    ROS_INFO("Hand: Get Position Set values request received");
+
+    // 直接读取各个手指的驱动器位置设置值
+    get_reg(setpos_, 1474); // 从寄存器 1474 开始读取
+
+    return validate_values(setpos_, 0, 2000); // 检查读取的值是否有效
+}
+
+bool hand_serial::get_actual_position() {
+    ROS_INFO("Hand: Get Position Actual values request received");
+
+    // 直接读取各个手指的驱动器实际位置值
+    get_reg(curpos_, 1534); // 从寄存器 1534 开始读取
+
+    return validate_values(curpos_, 0, 2000); // 检查读取的值是否有效
+}
+
+// Callback to set ID
+bool hand_serial::set_id(int id) {
+    ROS_INFO("Hand: Set ID request received");
+
+    // 检查请求中的 ID 是否在合法范围内
+    if (id >= 1 && id <= 254) {
+        // 将 ID 写入 Modbus 寄存器
+        int write_result = writeRegister(1000, id);
+        
+        if (write_result == 0) {
+            // 读取寄存器以验证写入的 ID
+            int read_value = readRegister(1000);
+            if (read_value != -1) {
+                ROS_INFO("Read ID from Modbus register: %d", read_value);
+                if (read_value == id) {
+                    ROS_INFO("ID verification successful.");
+                } else {
+                    ROS_WARN("ID verification failed! Expected: %d, Read: %d", id, read_value);
+                    return false;
+                }
+            }
+        }
+    }
+    return true; // 返回成功
+}
+
+// Callback to set Reduction Ratio
+bool hand_serial::set_redu_ratio(int redu_ratio) {
+    ROS_INFO("Hand: Set Reduction Ratio request received");
+
+    // 检查请求中的 redu_ratio 是否在合法范围内
+    if (redu_ratio >= 0 && redu_ratio <= 4) {
+        // 将 redu_ratio 写入 Modbus 寄存器
+        int write_result = writeRegister(1002, redu_ratio);
+        
+        if (write_result == 0) {
+            // 读取寄存器以验证写入的 redu_ratio
+            int read_value = readRegister(1002);
+            if (read_value != -1) {
+                ROS_INFO("Read REDU_RATIO from Modbus register: %d", read_value);
+                if (read_value == redu_ratio) {
+                    ROS_INFO("Reduction Ratio verification successful.");
+                } else {
+                    ROS_WARN("Reduction Ratio verification failed! Expected: %d, Read: %d", redu_ratio, read_value);
+                    return false;
+                }
+            } 
+            return true; // 返回成功
+        }
+    }
+}
+
+bool hand_serial::set_gesture_number(int gesture_no)
+{
+    int register_address = 0x0910;  // 当前动作序列索引寄存器
+    int action_register_address = 0x0912; // 动作序列号寄存器地址
+
+    // 首先将手势编号写入 Modbus 寄存器 2320
+    if (writeRegister(register_address, gesture_no) == -1) {
+        ROS_ERROR("Failed to set gesture number: %d", gesture_no);
+        return false; // 写入失败
+    }
+
+    // 然后写入执行动作序列号的寄存器 2322
+    if (writeRegister(action_register_address, 1) == -1) {
+        ROS_ERROR("Failed to execute action sequence for gesture number: %d", gesture_no);
+        return false; // 写入失败
+    }
+
+    return true; // 写入成功
+}
+
+bool hand_serial::validate_values(const double values[6], double lower_limit, double upper_limit) {
+    // 检查请求中的位置参数是否合法
+    for(int i = 0; i < 6; i++) {
+        if (values[i] < lower_limit || values[i] > upper_limit) {
+            ROS_WARN("Hand: value error! Values must be >= %f and <= %f.", lower_limit, upper_limit);
+            return false; // 返回失败
+        }
+    }
+    return true; // 返回成功
+}
+
+bool hand_serial::set_reg(const double values[6], int pin) {
+    // 将位置值写入 Modbus 寄存器
+    bool status = true;
+    for(int i = 0; i < 6; i++) {
+        status = status && (writeRegister(pin + i * 2, values[i]) == 0);
+    }
+    // 读取某个寄存器的值（位置0）
+    int read_value = readRegister(pin);
+    if (read_value != -1) {
+        ROS_INFO("Read value: %d", read_value);
+    }
+    return status; // 返回成功
+}
+
+bool hand_serial::save_setting() {
+    // 写入寄存器 1005 以保存设置
+    uint16_t save_value = 1; // 代表保存设置
+    int save_rc = writeRegister(1005, save_value);
+    if (save_rc == -1) {
+        ROS_ERROR("Failed to write to Modbus register 1005 to save settings");
+        return false; // 返回失败
+    }
+    return true; // 返回成功
+}
+
+// Callback to set position
+bool hand_serial::set_position(const double pos[6]) {
+    ROS_INFO("hand: set pos");
+    if(validate_values(pos, 0, 2000)) {
+        return set_reg(pos, 1474); // 返回成功
+    }
+    return false; // 返回失败
+}
+
+bool hand_serial::set_speed(const double speed[6]) {
+    ROS_INFO("hand: set speed");
+
+    if(validate_values(speed, 0, 1000)) {
+        return set_reg(speed, 1522); // 返回成功
+    }
+}
+
+bool hand_serial::set_default_speed(const double speed[6]) {
+    ROS_INFO("hand: set default speed");
+    if(validate_values(speed, 0, 1000) && set_reg(speed, 1032)) {
+        return save_setting(); // 返回成功
+    }
+    return false; // 返回失败
+}
+
+bool hand_serial::set_angle(const double angle[6]) {
+    ROS_INFO("hand: set angle");
+
+    if(validate_values(angle, -1, 1000)) {
+        return set_reg(angle, 1486); // 返回成功
+    }
+    return false; // 返回失败
+}
+
+bool hand_serial::set_force(const double force[6]) {
+    ROS_INFO("hand: set force");
+
+    if(validate_values(force, 0, 3000)) {
+        return set_reg(force, 1498); // 返回成功
+    }
+    return false; // 返回失败
+}
+
+bool hand_serial::set_default_force(const double force[6]) {
+    ROS_INFO("Hand: Set Default Force request received");
+
+    if(validate_values(force, 0, 3000) && set_reg(force, 1044)) {
+        return save_setting(); // 返回成功
+    }
+    return false; // 返回失败
+}
+
+bool hand_serial::set_force_calibration() {
+    ROS_INFO("Hand: Set Force Calibration request received");
+
+    uint16_t calibration_value = 1000; // 要写入的校准值
+
+    // 写入寄存器 1486 到 1496
+    for (int register_address = 1486; register_address <= 1496; register_address += 2) {
+        int rc = writeRegister(register_address, calibration_value);
+        if (rc == -1) {
+            ROS_ERROR("Failed to write to Modbus register %d: %s", register_address, modbus_strerror(errno));
+            return false; // 返回失败
+        }
+    }
+
+    // 延时 10 毫秒
+    usleep(10000); // 10ms
+
+    // 向寄存器 1009 写入 1，进行力控校准
+    uint16_t force_calibration_value = 1;
+    int rc = writeRegister(1009, force_calibration_value);
+    
+    if (rc == -1) {
+        ROS_ERROR("Failed to write to Modbus register 1009: %s", modbus_strerror(errno));
+        return false; // 返回失败
+    }
+    
+    return true; // 返回成功
+}
+
+bool hand_serial::set_current_limit(const double current_limit[6]) {
+    ROS_INFO("Hand: Set Current Limit request received");
+
+    if(validate_values(current_limit, 0, 1500)) {
+        return set_reg(current_limit, 1020); // 返回成功
+    }
+    return false; // 返回失败
+}
+
+bool hand_serial::set_clear_error() {
+    ROS_INFO("Hand: Set CLEAR ERROR request received");
+
+    uint16_t value = 1; // 写入1，代表清除错误
+    int rc = writeRegister(1004, value);
+    
+    if (rc == -1) {
+        ROS_ERROR("Failed to write to Modbus register: %s", modbus_strerror(errno));
+        return false;
+    }
+    return true; // 返回成功
+}
+
+// Callback to reset parameters
+bool hand_serial::set_reset_parameters() {
+    ROS_INFO("Hand: Set RESET PARAMETER request received");
+
+    uint16_t value = 1; // 写入1，代表重置参数
+    int rc = writeRegister(1006, value);
+    
+    if (rc == -1) {
+        ROS_ERROR("Failed to write to Modbus register: %s", modbus_strerror(errno));
+        return false; // 返回失败
+    }
+    return true; // 返回成功
+}
+
+// Read a register
+int hand_serial::readRegister(int reg_addr) {
+    std::lock_guard<std::mutex> lk(cmd_mutex_);
+    uint16_t value;
+    if (modbus_read_registers(ctx_, reg_addr, 1, &value) == -1) { // 只读取一个寄存器
+        ROS_ERROR("Failed to read register %d: %s", reg_addr, modbus_strerror(errno));
+        return -1; // Error
+    }
+    return value; // 返回读取的值
+}
+
+int hand_serial::readRegisters(int reg_addr, int num_registers, uint16_t *tab_reg) {
+    std::lock_guard<std::mutex> lk(cmd_mutex_);
+    if (modbus_read_registers(ctx_, reg_addr, num_registers, tab_reg) == -1) {
+        ROS_ERROR("Failed to read registers starting at %d: %s", reg_addr, modbus_strerror(errno));
+        return -1; // Error
+    }
+    return 0; // Success
+}
+
+// Write to a register
+int hand_serial::writeRegister(int reg_addr, int value) {
+    std::lock_guard<std::mutex> lk(cmd_mutex_);
+    if (modbus_write_register(ctx_, reg_addr, value) == -1) {
+        ROS_ERROR("Failed to write register %d: %s", reg_addr, modbus_strerror(errno));
+        return -1; // Error
+    }
+    return 0; // Success
+}
+
+// Write multiple registers (optional, if needed)
+int hand_serial::writeMultipleRegisters(int start_addr, const uint16_t *values, int num_values) {
+    std::lock_guard<std::mutex> lk(cmd_mutex_);
+    if (modbus_write_registers(ctx_, start_addr, num_values, values) == -1) {
+        ROS_ERROR("Failed to write registers starting at %d: %s", start_addr, modbus_strerror(errno));
+        return -1; // Error
+    }
+    return 0; // Success
+}
+}
+#endif
