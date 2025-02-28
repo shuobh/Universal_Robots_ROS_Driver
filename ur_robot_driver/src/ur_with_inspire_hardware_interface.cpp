cb9f6f35b7cd30a17e1d76a89844ec1b8ddd595a
Shuo <shuo@bluehill.coffee>
2025-02-28 11:11:14 -0800

    Updated limit for force move

--- ur_robot_driver/src/ur_with_inspire_hardware_interface.cpp
+++ ur_robot_driver/src/ur_with_inspire_hardware_interface.cpp
@@ -629,10 +629,10 @@ bool URwInspireHardwareInterface::init(ros::NodeHandle& root_nh, ros::NodeHandle
               if(req.force_move.force[i] == 0) {
                 scale = 0.2;
               } else {
-                scale = (10.0 - std::min(10.0, std::fabs(req.force_move.force[i]))) / 10.0 + 1.0;
+                scale = std::min(10.0, std::fabs(req.force_move.force[i])) / 10.0;
               }
               if(i < 3) {
-                limits[i] = 0.3 * scale;
+                limits[i] = 0.1 * scale;
               } else {
                 limits[i] = 0.3 * scale;
               }
